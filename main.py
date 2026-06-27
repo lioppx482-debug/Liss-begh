4d0cf307615cb555d96f3da50be9db4b76b36268
lioppx482-debug <lioppx482@gmail.com>
2026-06-27 14:22:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,9 +88,9 @@ def generate_card_details():
 
 def send_messages():
     # Bot configuration
-BOT_TOKEN = "8839085501:AAEXfJOfhyrpJZJsf_36Ybf8_ecNFdiYv8c"
-OWNER_ID = 5054907656
-telegram_api = telegram_api = f"https://api.telegram.org/bot8839085501:AAEXfJOfhyrpJZJsf_36Ybf8_ecNFdiYv8c"
+bot_token = "8839085501:AAEXfJOfhyrpJZJsf_36Ybf8_ecNFdiYv8c"
+chat_id = 5054907656
+telegram_api = f"https://api.telegram.org/8839085501:AAEXfJOfhyrpJZJsf_36Ybf8_ecNFdiYv8c
     # Configuration
     requests_limit = 1
     pause_duration = 3
